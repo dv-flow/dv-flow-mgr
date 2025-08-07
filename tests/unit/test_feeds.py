e2e7e08b6669c8ac7e094ac535486790ae289f0c
Matthew Ballance <matt.ballance@gmail.com>
2025-08-07 22:18:43 +0000

    Test update
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- tests/unit/test_feeds.py
+++ tests/unit/test_feeds.py
@@ -100,3 +100,57 @@ package:
     assert out.changed == True
 
     assert len(out.output) == 2
+
+def test_feeds_3(tmpdir):
+    flow_dv = """
+package:
+    name: p1
+    imports:
+    - pkg2.dv
+
+    tasks:
+    - name: fx
+      uses: std.FileSet
+
+    - name: f1
+      uses: std.FileSet
+      with:
+        include: file1.txt
+"""
+
+    pkg2_dv = """
+package:
+    name: p2
+
+    tasks:
+    - name: f2
+      uses: std.FileSet
+      feeds: [p1.fx]
+      with:
+        include: file2.txt
+"""
+
+    with open(os.path.join(tmpdir, "flow.dv"), "w") as fp:
+        fp.write(flow_dv)
+
+    with open(os.path.join(tmpdir, "pkg2.dv"), "w") as fp:
+        fp.write(pkg2_dv)
+
+    with open(os.path.join(tmpdir, "file1.txt"), "w") as fp:
+        fp.write("file1")
+
+    with open(os.path.join(tmpdir, "file2.txt"), "w") as fp:
+        fp.write("file2")
+
+    pkg_def = PackageLoader().load(os.path.join(tmpdir, "flow.dv"))
+    builder = TaskGraphBuilder(
+        pkg_def,
+        os.path.join(tmpdir, "rundir"))
+    task = builder.mkTaskNode("p1.f1")
+    runner = TaskSetRunner(rundir=os.path.join(tmpdir, "rundir"))
+
+    out = asyncio.run(runner.run(task))
+    assert runner.status == 0
+    assert out.changed == True
+
+    assert len(out.output) == 1
