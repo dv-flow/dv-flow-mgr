6c33d6584519c47812501a9c697e2412320a1e63
Matthew Ballance <matt.ballance@gmail.com>
2025-11-28 13:18:05 -0800

    Ensure that pytasks get updated env
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/dv_flow/mgr/pytask_callable.py
+++ src/dv_flow/mgr/pytask_callable.py
@@ -4,6 +4,23 @@ from typing import ClassVar, List
 from .task_data import TaskDataResult
 from .pytask import PyTask
 
+def _merge_env_filesets(ctxt, input):
+    """Collect std.Env filesets from inputs and merge them into the context environment."""
+    env = ctxt.env.copy()
+    # Merge std.Env data items from inputs
+    # Collect all std.Env vals in dependency order, oldest first
+    env_items = []
+    for item in getattr(input, "inputs", []):
+        if getattr(item, "type", None) == "std.Env" and hasattr(item, "vals") and isinstance(item.vals, dict):
+            env_items.append(item.vals)
+    # Merge all keys from all std.Env, oldest first, newest override
+    merged_env = {}
+    for vals in env_items:
+        for k, v in vals.items():
+            merged_env[k] = v
+    env.update(merged_env)
+    return env
+
 @dc.dataclass
 class PytaskCallable(object):
     run : str
@@ -12,6 +29,11 @@ class PytaskCallable(object):
     async def __call__(self, ctxt, input):
         self._log.debug("--> ExecCallable")
         self._log.debug("Body:\n%s" % "\n".join(self.body))
+
+        # Merge std.Env filesets into context environment for exec
+        env = _merge_env_filesets(ctxt, input)
+        ctxt.env.update(env)
+
         method = "async def pytask(ctxt, input):\n" + "\n".join(["    %s" % l for l in self.body])
 
         exec(method)
@@ -31,6 +53,11 @@ class PytaskClassCallable(object):
 
     async def __call__(self, ctxt, input):
         self._log.debug("--> PyTask")
+
+        # Merge std.Env filesets into context environment for exec
+        env = _merge_env_filesets(ctxt, input)
+        ctxt.env.update(env)
+
         self.run._ctxt = ctxt
         self.run._input = input
 
